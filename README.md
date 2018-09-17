0cd7bb59f7e13e0b8a52379522bd80455d58864e
Andrew Sellers <andrew@andrewcsellers.com>
2018-09-17 12:12:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Docuproxy
+
+We were using docusign with the node api, and we had some specific
+requirements about creating documents, and the official node
+library just wouldn't create them. So we made a temp command app
+using an unofficial go library that seemed to work!
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jfcote87/docusign"
+)
+
+var (
+	svc  *docusign.Service
+	demo *docusign.Template
+	user = flag.String("username", "", "UserName for docusign")
+	pass = flag.String("password", "", "Password for docusign")
+	acct = flag.String("accountid", "", "AccountID for docusign")
+	key  = flag.String("key", "", "Integrator Key for docusign")
+	host = flag.String("host", "demo.docusign.net", "Host (demo or prod)")
+)
+
+func Connect() {
+	cfg := &docusign.Config{
+		UserName:      *user,
+		Password:      *pass,
+		IntegratorKey: *key,
+		AccountId:     *acct,
+		Host:          *host,
+	}
+	svc = docusign.New(cfg, "")
+}
+
+func main() {
+	flag.Parse()
+	Connect()
+
+	env := &docusign.Envelope{}
+	err := json.NewDecoder(os.Stdin).Decode(env)
+	if err != nil {
+		log.Fatal("Cannot decode input")
+	}
+	saved, err := svc.EnvelopeCreate(
+		context.Background(),
+		env,
+	)
+	if err != nil {
+		fmt.Println(saved)
+		log.Fatal("Env Create: ", err)
+	}
+	json.NewEncoder(os.Stdout).Encode(saved)
+}
