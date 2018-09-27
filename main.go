baf8851353dd9359b6f150582231cbf2fb338e22
Andrew Sellers <andrew@andrewcsellers.com>
2018-09-27 14:32:36 -0500

    Docusign production may require a login info thing

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/acsellers/docusign"
 )
@@ -18,7 +20,7 @@ var (
 	pass = flag.String("password", "", "Password for docusign")
 	acct = flag.String("accountid", "", "AccountID for docusign")
 	key  = flag.String("key", "", "Integrator Key for docusign")
-	host = flag.String("host", "demo.docusign.net", "Host (demo or prod)")
+	host = flag.String("host", "demo.docusign.net", "Host (demo.docusign.net or www.docusign.net)")
 )
 
 func Connect() {
@@ -30,6 +32,26 @@ func Connect() {
 		Host:          *host,
 	}
 	svc = docusign.New(cfg, "")
+	if !strings.Contains(*host, "demo") {
+		li, err := svc.LoginInformation(
+			context.Background(),
+			docusign.LoginInfoParam{Name: "api_password", Value: "true"},
+		)
+		if err != nil {
+			log.Fatal("Login Info: ", err)
+		}
+		if len(li.LoginAccounts) == 0 {
+			log.Fatal("No Login Accounts Available?")
+		}
+		u, _ := url.Parse(li.LoginAccounts[0].BaseUrl)
+		svc = docusign.New(&docusign.Config{
+			UserName:      li.LoginAccounts[0].UserId,
+			Password:      li.ApiPassword,
+			IntegratorKey: *key,
+			AccountId:     li.LoginAccounts[0].AccountId,
+			Host:          u.Host,
+		}, "")
+	}
 }
 
 func main() {
@@ -39,7 +61,7 @@ func main() {
 	env := &docusign.Envelope{}
 	err := json.NewDecoder(os.Stdin).Decode(env)
 	if err != nil {
-		log.Fatal("Cannot decode input")
+		log.Fatal("Cannot decode input: ", err)
 	}
 	saved, err := svc.EnvelopeCreate(
 		context.Background(),
